90d0a874e78e7f0bb347f03a13f18879a7daa9a0
Jack Zemlanicky <jackzemlanicky@gmail.com>
2021-07-02 15:56:09 -0400

    added trainer class

--- lab4.py
+++ lab4.py
@@ -1,10 +1,22 @@
 from pytorch_lightning.trainer.trainer import Trainer
 import torchvision.models as models
+from torchvision.models.vgg import vgg11
 import byteplot as bp
 import pytorch_dataset_template as dataset
 import pytorch_lightning as pl
-m = models.vgg11(False,True)
-print(m)
+from torch.optim import Adam, optimizer
+
+
+class VGGTrainer(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.model = vgg11(False,True)
+    def forward(self,x):
+        x =self.model(x)
+        return
+    def configure_optimizers(self):
+        optimizer = Adam(self.model.parameters(),lr=self.args.lr,weight_decay=self.args.weight_decay)
+        return {'optimizer':optimizer}
+
 dataset.testcase_test_pdfdataset()
 trainer =Trainer
-trainer.fit(m,dataset,dataset)
